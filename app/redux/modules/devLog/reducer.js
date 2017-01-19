45c73e82e0e03c8cea76963acf7009eaaa31ec9e
ArnoldasSid <arnoldassid@gmail.com>
2017-01-19 08:50:36 +0200

    Fliped devLog order

--- app/redux/modules/devLog/reducer.js
+++ app/redux/modules/devLog/reducer.js
@@ -19,7 +19,7 @@ export default function devLogReducer (state: any = initialState, action: any) {
     const fullTitle = type ? `[${type}] ${title}` : title
     const stringMsg = (typeof message !== 'string') ? JSON.stringify(message, null, 2) : message
     return R.evolve({
-      messages: R.append({
+      messages: R.prepend({
         title: fullTitle,
         message: stringMsg,
         timestamp: new Date().valueOf(),
@@ -27,14 +27,14 @@ export default function devLogReducer (state: any = initialState, action: any) {
     }, state)
   } else if (action.type === DDP_CONNECTED) {
     return R.evolve({
-      messages: R.append({
+      messages: R.prepend({
         title: '[DDP] DDP Connected',
         timestamp: new Date().valueOf(),
       }),
     }, state)
   } else if (action.type === DDP_DISCONNECTED) {
     return R.evolve({
-      messages: R.append({
+      messages: R.prepend({
         title: '[DDP] DDP Disconnected',
         timestamp: new Date().valueOf(),
       }),
